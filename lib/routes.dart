3cebd0e450321f2365be30acb888b047af035897
jocgomez <37158004+jocgomez@users.noreply.github.com>
2021-02-17 19:24:49 -0500

    First commit

---
+++ lib/routes.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+Map<String, WidgetBuilder> getApplicationRoutes() => <String, WidgetBuilder>{
+      /* '/': (BuildContext context) => HomePage(), */
+/*       'alert': (BuildContext context) => AlertPage(),
+      'avatar': (BuildContext context) => AvatarPage(),
+      'card': (BuildContext context) => CardPage(),
+      'animatedContainer': (BuildContext context) => AnimatedContainerPage(),
+      'inputs': (BuildContext context) => InputPage(),
+      'slider': (BuildContext context) => SliderPage(),
+      'list': (BuildContext context) => ListaPage(), */
+    };
---
+++ lib/view/utils/style.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class StylesElements {
+  static Color colorPrimary = new Color(0xFFfd7e14);
+  static Color colorSecondary = new Color(0xFFfd7e14);
+}
